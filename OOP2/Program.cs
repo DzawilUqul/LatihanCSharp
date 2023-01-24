27b1d89589e483084174bbbd80d06517a5be6c36
DzawilUqul <112853502+DzawilUqul@users.noreply.github.com>
2023-01-24 10:56:26 +0700

    Adding Constructor

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace OOP2
+﻿using System;
+
+namespace OOP2
 {
+    class AddConstructor
+    {
+        public string brand;
+        public int speed;
+        public int year;
+
+        public AddConstructor(string cBrand, int cSpeed, int cYear)
+        {
+            brand = cBrand;
+            speed = cSpeed;
+            year = cYear;
+        }
+    }
+
+
     internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hello, World!");
+            AddConstructor Car1 = new AddConstructor("Ferrari",100,2000);
+            Console.WriteLine(Car1.brand + ", " + Car1.speed + ", " + Car1.year );
         }
     }
 }
